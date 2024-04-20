a6198619a40dcfec52488ce14021b7192f121465
Ronho <ronholzapfel@outlook.de>
2024-04-20 16:17:25 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "config.h"
+
+int main() {
+    std::cout << "Hello World! You are running version " << EvoRob_VERSION_MAJOR << "." << EvoRob_VERSION_MINOR << std::endl;
+    return 0;
+}
