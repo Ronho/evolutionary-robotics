c3c28e88d0def1e9a10b3eab72d41146ecc502bc
Ron Holzapfel <ron.holzapfel@deutschebahn.com>
2024-04-22 10:00:37 +0200

    feat: add light and correct drawing

--- main.cpp
+++ main.cpp
@@ -1,36 +1,94 @@
 #include <iostream>
 #include <matplot/matplot.h>
 #include <set>
+#include <cmath>
 #include "config.h"
 
-class Robot {
+class Light {
 public:
-    // Where the robot is located.
     std::array<double, 2> position;
-    // Where the robot is heading.
-    double angle;
-    // Radius (just for visualization).
     double radius;
+
+public:
+    Light(std::array<double, 2> position, double radius) {
+        this->position = position;
+        this->radius = radius;
+    }
+
+    void draw() {
+        auto a = matplot::ellipse(this->position[0]-(this->radius / 2), this->position[1]-(this->radius / 2), this->radius, this->radius);
+        a->fill(true);
+        a->color("green");
+    }
+};
+
+// Convert an angle (in degrees) to a vector in two dimensional space.
+std::vector<double> angleToVector(double angle, double vectorLength = 1) {
+    double x = std::cos(angle * M_PI/180) * vectorLength/2;
+    double y = std::sin(angle * M_PI/180) * vectorLength/2;
+
+    return std::vector<double>{x, y};
+}
+
+class Robot {
+public:        // Access specifier
+    // Where the robot is located (x, y).
+    std::array<double, 2> position;
+    // Angle where the robot is heading relative to its center.
+    double heading;
+    // Radius (just for visualization). The center of the circle represents the robot itself.
+    double radius;
+    // Angles of the sensors relative to the angle of the heading;
+    std::array<double, 2> sensors;
     // Speed of the left (idx 0) and right (idx 1) wheel.
     std::array<double, 2> speed;
+
+public:
+    Robot(std::array<double, 2> position, double heading, double radius, std::array<double, 2> speed, std::array<double, 2> sensors) {
+        this->position = position;
+        this->heading = heading;
+        this->radius = radius;
+        this->speed = speed;
+        this->sensors = sensors;
+    }
+
+    std::vector<double> getHeadingVector() {
+        double x = std::cos(heading * M_PI/180) * radius/2;
+        double y = std::sin(heading * M_PI/180) * radius/2;
+
+        return std::vector<double>{x, y};
+    }
+
+    void draw() {
+        std::vector<double> vec = angleToVector(this->heading, this->radius);
+        matplot::ellipse(this->position[0]-(this->radius / 2), this->position[1]-(this->radius / 2), this->radius, this->radius);
+        // Heading direction.
+        auto a = matplot::arrow(this->position[0], this->position[1], this->position[0] + vec[0], this->position[1] + vec[1]);
+        a->color("red");
+        // Sensors.
+        vec = angleToVector(this->heading + this->sensors[0]);
+        matplot::arrow(this->position[0], this->position[1], this->position[0] + vec[0], this->position[1] + vec[1]);
+        vec = angleToVector(this->heading + this->sensors[1]);
+        matplot::arrow(this->position[0], this->position[1], this->position[0] + vec[0], this->position[1] + vec[1]);
+    }
 };
+
+
 
 int main()
 {
     std::cout << "You are running version " << EvoRob_VERSION_MAJOR << "." << EvoRob_VERSION_MINOR << "." << std::endl;
     try
     {
-        Robot rob;
-        rob.position = {0.5, 0.5};
-        rob.angle = 90;
-        rob.radius = 0.1;
-        // {0, 1};
-        double arrowLength = 0.3;
-        matplot::arrow(rob.position[0], rob.position[1], rob.position[0] + 0*arrowLength, rob.position[1] + arrowLength); // x1, y1, x2, y2
-        matplot::arrow(rob.position[0], rob.position[1], rob.position[0] + arrowLength, rob.position[1] + arrowLength); // x1, y1, x2, y2
-        matplot::arrow(rob.position[0], rob.position[1], rob.position[0] + (arrowLength*(22.5/90)), rob.position[1] + arrowLength); // x1, y1, x2, y2
-        matplot::arrow(rob.position[0], rob.position[1], rob.position[0] + (arrowLength*(22.5/90)), rob.position[1] - arrowLength); // x1, y1, x2, y2
-        matplot::ellipse(rob.position[0]-(rob.radius / 2), rob.position[1]-(rob.radius / 2), rob.radius, rob.radius); // x, y, w, h
+        Robot rob({0.5, 0.5}, 90, 0.5, {100.0, 100.0}, {-45, 45});
+        Light buzz({0.75, 0.75}, 0.1);
+        rob.draw();
+        buzz.draw();
+
+        // Sensor: needs an angle which is relative to the heading of the robot.
+        // Get distance between the sensor and the light
+        // 1. Create
+        
         matplot::xlim({0, 1});
         matplot::ylim({0, 1});
         matplot::show();
