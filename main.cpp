4e244f94c40f8b66fd1ff1dc905b733af38ef164
Ronho <ronholzapfel@outlook.de>
2024-04-26 17:05:05 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#define _USE_MATH_DEFINES
+#define ERROR_BOUND 1e-5
+
 #include <iostream>
 #include <matplot/matplot.h>
 #include <set>
@@ -23,11 +26,22 @@ public:
     }
 };
 
+
+inline bool isNearlyEqual(double x, double y)
+{
+  return std::abs(x - y) <= ERROR_BOUND;
+}
+
 // Convert an angle (in degrees) to a vector in two dimensional space.
 std::vector<double> angleToVector(double angle, double vectorLength = 1) {
-    double x = std::cos(angle * M_PI/180) * vectorLength/2;
-    double y = std::sin(angle * M_PI/180) * vectorLength/2;
-
+    double x = std::cos(angle/180 * M_PI) * vectorLength/2;
+    double y = std::sin(angle/180 * M_PI) * vectorLength/2;
+    if (isNearlyEqual(x, 0.0)) {
+        y *= 2;
+    }
+    if (isNearlyEqual(y, 0.0)) {
+        x *= 2;
+    }
     return std::vector<double>{x, y};
 }
 
@@ -56,6 +70,52 @@ public:
         this->radius = radius;
         this->speed = speed;
         this->sensors = sensors;
+    }
+
+    void drive(std::vector<double> speed, double timeSteps) {
+        // Speed in unit/timeStep.
+        const double rotation_offset = (this->radius) * (speed[1] + speed[0])/(speed[1] - speed[0]);
+        const double rotation_angle = (360 * speed[1] * timeSteps)/(2 * M_PI * rotation_offset);
+        std::vector<double> rotation_point = angleToVector(this->heading + 90, rotation_offset);
+        this->drawArrowRelativeToCenter(rotation_point);
+
+        const double heading_angle_offset = (180 - rotation_angle) / 2;
+        const double heading_length = rotation_offset / std::tan(heading_angle_offset);
+        const std::vector<double> new_position = angleToVector(this->heading + heading_angle_offset, heading_length);
+        this->position[0] += new_position[0];
+        this->position[1] += new_position[1];
+        this->heading += rotation_angle;
+
+
+        std::cout << rotation_angle << " " << rotation_offset  << " " << heading_length << " " << heading_angle_offset << " " << speed[0] * timeSteps << std::endl;
+        // double dl = speed[0] * timeSteps;
+        // double dr = speed[1] * timeSteps;
+
+        
+        
+        // double x = rotation_offset * std::cos(dr / (this->radius + rotation_offset)) - rotation_offset;
+        // double y = rotation_offset * std::sin(dr / (this->radius + rotation_offset));
+        // std::cout << new_angle << " " << rotation_offset << " " << x << " " << y << std::endl;
+
+
+
+        // this->heading = this->heading - new_angle;
+
+        // this->position[0] = this->position[0] + x - (this->radius / 2);
+        // this->position[1] = this->position[1] + y -(this->radius / 2);
+        
+        // std::vector<double> vec = angleToVector(this->heading + 90, s);
+        // std::cout << "S: " << s << " x" << vec[0] << vec[1] << std::endl;
+        // this->drawArrowRelativeToCenter(vec);
+
+
+        // double x = s * std::cos(dr / (this->radius + s)) - s;
+        // double y = s * std::sin(dr / (this->radius + s));
+
+        // // this->position
+
+        // matplot::ellipse(this->position[0] + x - (this->radius / 2), this->position[1] + y -(this->radius / 2), this->radius, this->radius);
+        
     }
 
     std::vector<double> getHeadingVector() {
@@ -89,9 +149,13 @@ int main()
     std::cout << "You are running version " << EvoRob_VERSION_MAJOR << "." << EvoRob_VERSION_MINOR << "." << std::endl;
     try
     {
-        Robot rob({0.5, 0.5}, 90, 0.5, {100.0, 100.0}, {-45, 45});
+        Robot rob({0.1, 0.1}, 90, 0.5, {100.0, 100.0}, {-45, 45});
         Light buzz({0.75, 0.75}, 0.1);
         rob.draw();
+        for (int i = 0; i <= 100; i++) {
+            rob.drive({0.075, 0.01}, 0.1);
+            rob.draw();
+        }
         buzz.draw();
 
         std::vector<double> vec = {buzz.position[0] - rob.position[0], buzz.position[1] - rob.position[1]};
@@ -103,8 +167,8 @@ int main()
         // Get distance between the sensor and the light
         // 1. Create
         
-        matplot::xlim({0, 1});
-        matplot::ylim({0, 1});
+        matplot::xlim({-2, 2});
+        matplot::ylim({-2, 2});
         matplot::show();
     }
     catch (const std::runtime_error &e)
