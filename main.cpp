9cd76245eee434d8f1dbda0e046504b0411a0023
Ronho <ronholzapfel@outlook.de>
2024-04-27 11:52:02 +0200

    update

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <stdexcept>
 #include "config.h"
 
+// TODO
 class Light {
 public:
     std::array<double, 2> position;
@@ -20,34 +21,33 @@ public:
         this->radius = radius;
     }
 
-    void draw() {
-        auto a = matplot::ellipse(this->position[0]-(this->radius / 2), this->position[1]-(this->radius / 2), this->radius, this->radius);
-        a->fill(true);
-        a->color("green");
-    }
+    // double getIntensity(std::vector<double> sensor) {
+    // }
 };
 
 
-inline bool isNearlyEqual(double x, double y)
-{
-  return std::abs(x - y) <= ERROR_BOUND;
-}
-
-// Convert an angle (in degrees) to a vector in two dimensional space.
+// Convert an angle to a vector in two dimensional space.
+//
+// Args:
+// - angle: Angle in degrees.
+// - vectorLength: Length the vector should have.
+//
+// Returns:
+//   Vector relative to the [0, 0] vector with the given length.
 std::vector<double> angleToVector(double angle, double vectorLength = 1) {
     double x = std::cos(angle/180 * M_PI) * vectorLength/2;
     double y = std::sin(angle/180 * M_PI) * vectorLength/2;
-    if (isNearlyEqual(x, 0.0)) {
-        y *= 2;
-    }
-    if (isNearlyEqual(y, 0.0)) {
-        x *= 2;
-    }
     return std::vector<double>{x, y};
 }
 
+
+// TODO
+bool isNearlyEqual(double a, double b) {
+    return std::abs(a-b) < ERROR_BOUND;
+}
+
 class Robot {
-public:        // Access specifier
+public:
     // Where the robot is located (x, y).
     std::array<double, 2> position;
     // Angle where the robot is heading relative to its center.
@@ -56,8 +56,6 @@ public:        // Access specifier
     double radius;
     // Angles of the light sensors relative to the angle of the heading;
     std::array<double, 2> sensors;
-    // Speed of the left (idx 0) and right (idx 1) wheel.
-    std::array<double, 2> speed;
 
 private:
     auto drawArrowRelativeToCenter(std::vector<double> vec) {
@@ -65,103 +63,106 @@ private:
     }
 
 public:
-    Robot(std::array<double, 2> position, double heading, double radius, std::array<double, 2> speed, std::array<double, 2> sensors) {
+    // Constructor
+    //
+    // Args
+    // - position: Initial position of the robot in a two dimensional space.
+    // - heading: The way, the robot is heading based on its center/position (in degree).
+    // - radius: Size of the robot.
+    // - sensors: Angle where the sensors are located relative to the heading.
+    Robot(std::array<double, 2> position, double heading, double radius, std::array<double, 2> sensors) {
         this->position = position;
         this->heading = heading;
         this->radius = radius;
-        this->speed = speed;
         this->sensors = sensors;
     }
 
+    // Drive the vehicle.
+    // 
+    // Args:
+    // - speed: Speed of the wheels in unit/timeStep.
+    // - timeSteps: Number of time steps to take. With increasing timeSteps the accuracy of the update is reduced.
     void drive(std::vector<double> speed, double timeSteps) {
-        // Speed in unit/timeStep.
-        const double rotation_offset = (this->radius) * (speed[1] + speed[0])/(speed[1] - speed[0]);
-        const double rotation_angle = (360 * speed[1] * timeSteps)/(2 * M_PI * rotation_offset);
-        #ifdef DEBUG
-            std::vector<double> rotation_point = angleToVector(this->heading + 90, rotation_offset);
-            this->drawArrowRelativeToCenter(rotation_point);
-        #endif
+        std::vector<double> newPosition;
+        double rotationAngle;
+        if (isNearlyEqual(speed[0], speed[1])) {
+            // Straight forward.
+            rotationAngle = 0;
+            newPosition = angleToVector(this->heading + rotationAngle, speed[1] * timeSteps);
+        } else if (isNearlyEqual(-speed[0], speed[1])) {
+            // Rotate on spot.
+            rotationAngle = (360 * speed[1] * timeSteps)/(2 * M_PI * this->radius);
+            newPosition = {0, 0};
+        } else {
+            double rotationOffset = (this->radius) * (speed[1] + speed[0])/(speed[1] - speed[0]);
+            rotationAngle = (360 * speed[1] * timeSteps)/(2 * M_PI * rotationOffset);
+            const double headingAngleOffset = (180 - rotationAngle) / 2;
+            const double headingLength = rotationOffset / std::tan(headingAngleOffset * M_PI/180);
+            newPosition = angleToVector(this->heading + rotationAngle, headingLength);
+        }
 
-        const double heading_angle_offset = (180 - rotation_angle) / 2;
-        const double heading_length = rotation_offset / std::tan(heading_angle_offset * M_PI/180);
-        const std::vector<double> new_position = angleToVector(this->heading + rotation_angle, heading_length);
-        this->position[0] += new_position[0];
-        this->position[1] += new_position[1];
-        this->heading += rotation_angle;
-
-        #ifdef DEBUG
-            std::cout << rotation_angle << " " << rotation_offset  << " " << heading_length << " " << heading_angle_offset << " " << speed[0] * timeSteps << std::endl;
-        #endif
-        // double dl = speed[0] * timeSteps;
-        // double dr = speed[1] * timeSteps;
-
-        
-        
-        // double x = rotation_offset * std::cos(dr / (this->radius + rotation_offset)) - rotation_offset;
-        // double y = rotation_offset * std::sin(dr / (this->radius + rotation_offset));
-        // std::cout << new_angle << " " << rotation_offset << " " << x << " " << y << std::endl;
-
-
-
-        // this->heading = this->heading - new_angle;
-
-        // this->position[0] = this->position[0] + x - (this->radius / 2);
-        // this->position[1] = this->position[1] + y -(this->radius / 2);
-        
-        // std::vector<double> vec = angleToVector(this->heading + 90, s);
-        // std::cout << "S: " << s << " x" << vec[0] << vec[1] << std::endl;
-        // this->drawArrowRelativeToCenter(vec);
-
-
-        // double x = s * std::cos(dr / (this->radius + s)) - s;
-        // double y = s * std::sin(dr / (this->radius + s));
-
-        // // this->position
-
-        // matplot::ellipse(this->position[0] + x - (this->radius / 2), this->position[1] + y -(this->radius / 2), this->radius, this->radius);
-
-
-
-
-        // double angular_velocity = (speed[1]-speed[0])/(this->radius*2);
-        // double translation = ((speed[1]+speed[0])/2) * timeSteps;
-        // std::cout << "-----" << std::endl;
-        // std::cout << "Angular velocity: " << angular_velocity << std::endl;
-        // std::cout << "Heading before: " << this->heading << std::endl;
-        // std::cout << "Current position: " << this->position[0] << "," << this->position[1] << std::endl;
-        // this->heading += angular_velocity * timeSteps;
-        // std::cout << std::cos(this->heading) << std::endl;
-        // this->position[0] += translation * std::cos(this->heading * M_PI/180);
-        // this->position[1] += translation * std::sin(this->heading * M_PI/180);
-        // std::cout << "Next position: " << this->position[0] << "," << this->position[1] << std::endl;
-        // std::cout << "Heading after: " << this->heading << std::endl;
-        
+        this->position[0] += newPosition[0];
+        this->position[1] += newPosition[1];
+        this->heading += rotationAngle;
     }
 
+    // TODO
     std::vector<double> getHeadingVector() {
         return angleToVector(this->heading, this->radius);
     }
 
+    // TODO
     std::vector<double> getSensorVector(int idx) {
         if (idx > 1 || idx < 0) {
             throw std::invalid_argument("Index out of bounds");
         }
 
-        return angleToVector(this->heading + this->sensors[idx]);
-    }
-
-    void draw() {
-        std::vector<double> vec = this->getHeadingVector();
-        matplot::ellipse(this->position[0]-(this->radius / 2), this->position[1]-(this->radius / 2), this->radius, this->radius);
-        // Heading direction.
-        auto a = this->drawArrowRelativeToCenter(this->getHeadingVector());
-        a->color("red");
-        // Sensors.
-        this->drawArrowRelativeToCenter(this->getSensorVector(0));
-        this->drawArrowRelativeToCenter(this->getSensorVector(1));
+        return angleToVector(this->heading + this->sensors[idx], this->radius);
     }
 };
 
+// TODO
+class Visualizer {
+private:
+    matplot::figure_handle f;
+    matplot::axes_handle ax;
+
+private:
+    matplot::vectors_handle drawArrowRelativeToCenter(std::array<double, 2> center, std::vector<double> vec) {
+        matplot::vectors_handle v = this->ax->arrow(center[0], center[1], center[0] + vec[0], center[1] + vec[1]);
+        return v;
+    }
+
+public:
+    Visualizer() {
+        this->f = matplot::figure(true);
+        this->ax = this->f->current_axes();
+        this->ax->xlim({-100, 100});
+        this->ax->ylim({-100, 100});
+    }
+
+    void drawRobot(Robot *rob) {
+        this->ax->ellipse(rob->position[0]-(rob->radius / 2), rob->position[1]-(rob->radius / 2), rob->radius, rob->radius);
+        matplot::vectors_handle v = this->drawArrowRelativeToCenter(rob->position, rob->getHeadingVector());
+        v->color("red");
+        this->drawArrowRelativeToCenter(rob->position, rob->getSensorVector(0));
+        this->drawArrowRelativeToCenter(rob->position, rob->getSensorVector(1));
+    }
+
+    void drawLight(Light *light) {
+        auto a = this->ax->ellipse(light->position[0]-(light->radius / 2), light->position[1]-(light->radius / 2), light->radius, light->radius);
+        a->fill(true);
+        a->color("green");
+    }
+
+    void show() {
+        this->f->show();
+    }
+
+    void save(std::string filename) {
+        this->f->save(filename);
+    }
+};
 
 
 int main()
@@ -169,27 +170,22 @@ int main()
     std::cout << "You are running version " << EvoRob_VERSION_MAJOR << "." << EvoRob_VERSION_MINOR << "." << std::endl;
     try
     {
-        Robot rob({0.1, 0.1}, 90, 0.5, {100.0, 100.0}, {-45, 45});
-        Light buzz({0.75, 0.75}, 0.1);
-        rob.draw();
-        for (int i = 0; i <= 25; i++) {
-            rob.drive({0.075, 0.1}, 1);
-            rob.draw();
+        Robot rob({0.5, 0.5}, 90.0, 5.0, {-45, 45});
+        Light buzz({40, 40}, 10);
+        Visualizer viz;
+
+        viz.drawRobot(&rob);
+        for (int i = 0; i < 100; i++) {
+            #ifdef DEBUG
+                std::cout << "Iteration: " << i << std::endl;
+            #endif
+
+            rob.drive({-1, 1}, 1);
+            viz.drawRobot(&rob);
         }
-        buzz.draw();
-
-        std::vector<double> vec = {buzz.position[0] - rob.position[0], buzz.position[1] - rob.position[1]};
-        std::cout << buzz.position[0] << " - " << buzz.position[1] << std::endl;
-        std::cout << rob.position[0] << " - " << rob.position[1] << std::endl;
-        std::cout << vec[0] << " - " << vec[1] << std::endl;
-
-        // Sensor: needs an angle which is relative to the heading of the robot.
-        // Get distance between the sensor and the light
-        // 1. Create
-        
-        matplot::xlim({-2, 2});
-        matplot::ylim({-2, 2});
-        matplot::show();
+        viz.drawLight(&buzz);
+        viz.save("path.svg");
+        viz.show();
     }
     catch (const std::runtime_error &e)
     {
